113570c4d7c8b040548bbe10fcfb608738271550
seth212 <seth@example.com>
2026-06-18 11:42:02 -0400

    added two middlewares to app

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(port, () =>{
     console.log('Server is running on port:', port)
